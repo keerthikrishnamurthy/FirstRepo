cdcbd3077fe96efd46160e999526a24914d12a6f
keerthikrish <keerthikrish2019@gmail.com>
2018-10-06 14:45:08 -0700

    Adding Yahoo Test

---
+++ YahooSearchTest.java
@@ -0,0 +1,45 @@
+package YahooSearch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class YahooSearchTest {
+private WebDriver driver;
+    @BeforeTest(groups = {"hello"})
+    public void setupDriver(){
+    System.setProperty("webdriver.gecko.driver","C:\\Users\\keert\\IdeaProjects\\WebDriverJavaBootCamp\\src\\test\\resources\\geckodriver.exe");
+    driver=new FirefoxDriver();
+
+    }
+    @Test(groups={"hello"})
+    public void navigatingToMainPage() {
+String url="http://yahoo.com";
+driver.get(url);
+
+    }
+    @Test(groups="hello")
+    public void testSearch() throws Exception {
+        navigatingToMainPage();
+        typeRequestInSearchInput();
+        submitSearch();
+        printOutAmountOfResult();
+    }
+    @Test
+    public void typeRequestInSearchInput(){
+        driver.findElement(YahooSearchMainPage.inputField).sendKeys("Portnov Computer School");
+
+    }
+
+    @Test
+    public void submitSearch(){
+        driver.findElement(YahooSearchMainPage.clickSubmit).click();
+    }
+
+    @Test
+    public void printOutAmountOfResult(){
+        String res= driver.findElement(YahooSearchMainPage.totalResult).getText();
+        System.out.println(res);
+    }
+}
